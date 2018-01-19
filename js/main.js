c37891d125ba1d9cf21b8d6fab89a5cc9dc17d09
blacksilla <afonsosilva1@ua.pt>
2018-01-19 17:51:06 +0000

    apenas alguns testes

--- js/main.js
+++ js/main.js
@@ -1,31 +1,71 @@
 /**
  * Created by blacksilla on 13/01/2018.
  */
-var x = document.createElement('script');
-x.src = "spriteSheet.js";
-document.getElementsByTagName("head")[0].appendChild(x);
+ window.onload=function () {
+    
+};
+
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d')
+    ;
+
+var img = new Image();
+img.src = '../img/sokoban_spritesheet.png';
 
 
-function loadInage(url) {
-    return new Promise(resolve=function(){
-            const image = new Image();
-    image.addEventListener("load",function () {
-        resolve(image);
-    });
-    image.src=url;
-        })
+var sprite = new Sprite("sokoban_spritesheet.png", 32, 16, [
+    // specify a few sprite locations
+    [10, 523],  // green
+    [131, 523], // pink
+    [191, 523]  // hit
+]);
+sprite.draw(0, 10, 200);
+sprite.draw(1, 50, 200);
+sprite.draw(2, 90, 200);
+
+
+var spriteWidth  = 350,
+    spriteHeight = 170,
+    pixelsLeft   = 170,
+    pixelsTop    = 10,
+
+// Where are we going to draw
+// the sprite on the canvas
+    canvasPosX   = 20,
+    canvasPosY   = 20
+    ;
+
+context.drawImage(img,
+    pixelsLeft,
+    pixelsTop,
+    spriteWidth,
+    spriteHeight,
+    canvasPosX,
+    canvasPosY,
+    spriteWidth,
+    spriteHeight
+);
+
+function Sprite(img, width, height, positions){
+    this.img = img;
+    this.width = width;
+    this.height = height;
+    this.positions = positions;
 }
+Sprite.prototype = {
+    draw: function(position, x, y){
+        var pos = this.positions[position];
+        context.drawImage(
+            this.img,
+            pos[0],
+            pos[1],
+            this.width,
+            this.height,
+            x, y,
+            this.width,
+            this.height
+        );
+    }
+};
 
 
-
-
-const canvas=document.getElementById("screen");
-const context=canvas.getContext("2d");
-
-loadInage("img/sokoban_spritesheet.png")
-.then(image)()
-    const sprites=new SpriteSheet(image,16,16);
-sprites.define("ground",0,0);
-sprites.draw("ground",context,45,62);
-    context.drawImage(image,0,0,16,16,0,0,16,16);
-})
