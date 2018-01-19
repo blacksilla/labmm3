a12293f1aaccd8fef23d24d7dfab24be9b8ea46b
blacksilla <afonsosilva1@ua.pt>
2018-01-19 19:04:49 +0000

    apenas alguns testes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
  */
 
 window.onload=function () {
+    window.onkeyup=function(e){processaSalto(e)};
     carregaElementos();
     document.getElementById("jogar").onclick=function () {
         jogar();
@@ -22,5 +23,24 @@ function carregaElementos(){
 function jogar() {
     document.getElementById("menu_inicial").style.visibility="hidden";
     document.getElementById("main").style.visibility="visible";
+    document.getElementById("screen").innerHTML=document.getElementById("screen").innerHTML + "<img id='jogador' src='img/personagem/andar/ELEMENTOS-02.png' />"
+
+}
+
+function processaSalto(e){
+    var tecla=e.key;
+    console.log("tecla",tecla);
+    if(tecla == " "){
+        salto=setInterval(function(){salta()},100);
+    }
+}
+
+function salta(){
+    var altura=parseInt(document.getElementById("homer").style.top);
+    console.log(document.getElementById("homer").style.top);
+    document.getElementById("homer").style.top=altura - 5 + "px";
+    if(altura==(195-65))
+        document.getElementById("homer").style.top=altura + 5 + "px";
+    clearInterval(salto);
 
 }
